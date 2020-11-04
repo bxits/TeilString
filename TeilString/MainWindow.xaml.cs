d5957408137f0558d0ce6205eeecf381690224f5
bxits <ruediger.berg@its-stuttgart.de>
2020-11-04 19:50:11 +0100

    Projektdateien hinzufügen.

---
+++ TeilString/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeilString
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSplit_Click(object sender, RoutedEventArgs e)
+        {
+            string textAusTextBox = txtEingabe.Text;
+
+            //Zeichenkette (String) zerlegen anhand des Zeichens ','.
+            //Es wird ein sog. Array zurückgegeben.
+            string[] stringTeile = textAusTextBox.Split(',');
+
+            //Auf die einzelnen Elemente in stringTeile zugreifen.
+            lblAusgabe.Content = stringTeile[0] + " ---" + stringTeile[1] + "---" + stringTeile[2];
+        }
+    }
+}
